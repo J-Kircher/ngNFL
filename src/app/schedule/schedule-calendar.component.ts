1099ffe3abd7a32a1f7ecc84d09eb5f59a55e80f
Jim <James.C.KIrcher@gmail.com>
2019-02-11 17:49:01 -0500

    Update schedule page to start on current game day

--- src/app/schedule/schedule-calendar.component.ts
+++ src/app/schedule/schedule-calendar.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { NFLCalendar } from '../model/nfl.model';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { NFLCalendar, ISchedule } from '../model/nfl.model';
+import { ScheduleService } from '../service/schedule.service';
+import { ScheduleDayService } from '../service/schedule.day.service';
 
 @Component({
   selector: 'schedule-calendar',
@@ -40,10 +42,31 @@ import { NFLCalendar } from '../model/nfl.model';
   `]
 })
 
-export class ScheduleCalenderComponent implements OnInit {
+export class ScheduleCalenderComponent implements OnInit, AfterContentInit {
   NFLCalendarArr: NFLCalendar[] = [];
+  gamesArr: ISchedule[] = [];
+
+  constructor(
+    private scheduleService: ScheduleService,
+    private scheduleDayService: ScheduleDayService
+  ) { }
 
   ngOnInit() {
+    // console.log('[schedule-calendar] ngOnInit()');
     this.NFLCalendarArr = [{ month: 9, year: 2017}, { month: 10, year: 2017}, { month: 11, year: 2017}, { month: 12, year: 2017}];
+  }
+
+  ngAfterContentInit() {
+    // console.log('[schedule-calendar] ngAfterContentInit()');
+    this.getGamesForDay(this.scheduleService.currentGameDay);
+  }
+
+  getGamesForDay(gameDay: string) {
+    // console.log('[schedule-calendar] getGamesForDay() gameDay: ' + gameDay);
+    this.gamesArr = this.scheduleService.getGamesForDay(gameDay);
+    if (this.gamesArr.length > 0) {
+      // console.log('[schedule-calendar] getGamesForDay() calling setScheduleDay()');
+      this.scheduleDayService.setScheduleDay(gameDay, this.gamesArr);
+    }
   }
 }
