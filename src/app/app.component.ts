63c35fb1023f31712d8c87d87d47ef428056136a
Jim <James.C.KIrcher@gmail.com>
2019-02-18 17:36:29 -0500

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,7 @@ import { TeamService } from './service/team.service';
 @Component({
   selector: 'app-nfl',
   templateUrl: './app.component.html',
-  styles: [`
-    .xtitle-well {
-      font-family: Arial;
-      font-style: italic;
-      font-size: 32px;
-      font-weight: bold;
-      text-shadow: 2px 2px 0 rgba(0,0,0,1);
-      padding-top: 0px;
-      padding-bottom: 0px;
-    }
-  `]
+  styles: []
 })
 
 export class AppComponent implements OnInit {
--- src/app/model/nfl.model.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=nfl.model.js.map
