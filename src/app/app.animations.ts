731d0d524b5ac5c040694ade2b3263cbba3b22d1
Jim <James.C.KIrcher@gmail.com>
2019-02-20 14:00:45 -0500

    Add a slide animation for future use

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -26,6 +26,24 @@ export const fadeAnimation =
     ])
   ]);
 
+  export class RouterAnimations {
+    static routeSlide =
+      trigger('routeSlide', [
+        transition('* <=> *', [
+          group([
+            query(':enter', [
+              style({transform: 'translateX({{offsetEnter}}%)'}),
+              animate('0.4s ease-in-out', style({transform: 'translateX(0%)'}))
+            ], {optional: true}),
+            query(':leave', [
+              style({transform: 'translateX(0%)'}),
+              animate('0.4s ease-in-out', style({transform: 'translateX({{offsetLeave}}%)'}))
+            ], {optional: true}),
+          ])
+        ]),
+      ]);
+  }
+
 // export const slideInAnimation =
 //   trigger('routeAnimations', [
 //     transition('HomePage <=> AboutPage', [
