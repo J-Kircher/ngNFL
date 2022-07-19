be3a70ec0cf7357182a529c342fa2887301c1835
Jim <James.C.KIrcher@gmail.com>
2022-07-19 11:51:10 -0400

    Update for 2022 schedule

--- src/app/schedule/schedule-calendar.component.ts
+++ src/app/schedule/schedule-calendar.component.ts
@@ -13,7 +13,7 @@ export class ScheduleCalenderComponent implements OnInit, AfterContentInit {
   calendarArr: NFLCalendar[] = [];
   gameDay: string;
   gamesArr: ISchedule[] = [];
-  scheduleYear: number = 2021;
+  scheduleYear: number = 2022;
   fullSchedule: ISchedule[];
 
   monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
